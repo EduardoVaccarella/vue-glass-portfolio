abc3f2e58555c82882358b6bfa3483974a93dee1
eduardoego <eduardo0vaccarella@gmail.com>
2022-10-03 15:47:39 +0200

    .

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ import NavBar from './components/NavBar.vue';
 import AboutMe from './components/AboutMe.vue';
 import AnimatedCards from './components/AnimatedCards.vue';
 import AnimatedHeader from './components/AnimatedHeader.vue';
-import MiniCards from './components/MiniCardsLoop.vue';
+import MiniCards from './components/MiniCards.vue';
 import FooterIcons from './components/FooterIcons.vue';
 import DownloadCv from './components/DownloadCv.vue';
 import MiniCardsLoop from './components/MiniCardsLoop.vue';
